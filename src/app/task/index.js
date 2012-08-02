fee9307f8910dcf76d28610da7567b4c45fac1c7
Kentaro Takiguchi <takiguchi0817@gmail.com>
2012-08-03 05:10:49 +0900

    タスクファイルを少し書きやすくした

--- src/app/task/index.js
+++ src/app/task/index.js
@@ -1,46 +1,23 @@
-var TaskManager = require("../../node_modules/webgrid/lib/TaskManager")
-var taskManager = new TaskManager()
+var onResult = function() {}
 
-module.exports.main = function() {
+module.exports.main = function(appDir) {
+  var fs = require('fs')
 
-  function primaryTest() {
-    var args = arguments[0]
-    var result = new Array()
+  var infile = appDir + "/task/main.js"
+  var outfile = appDir + "/task/.main.js"
+  var dat = 'var TaskManager = require("../../node_modules/webgrid/lib/TaskManager")' + '\n' + 'var taskManager = new TaskManager()' + '\n'
+  fs.readFileSync(infile).toString().split('\n').forEach(function (line) {
+    var newLine = line.replace(/#__PARALLEL__/, "taskManager.addTask")
+    dat = dat + newLine + '\n'
+  })  
+  fs.writeFileSync(outfile, dat)
 
-    for (var i = 0; i < args.length; i++) {
-      var num = args[i]
-      var flag = true
-      var limit = Math.sqrt(num)
-      for (var j = 2; j <= limit; j++) {
-        if (num % j == 0) {
-          flag = false
-          break
-        }
-      }
-      if (flag) {
-        result.push(num)
-      }
-    }
-    return result
-  }
+  var main = require(appDir + '/task/.main')
+  onResult = main.onResult
 
-  var dataNum = 100000
-  var range = 100
-  for (var i = 0; i < dataNum / range; i++) {
-    var args = new Array();
-    for (var j = 0; j < range; j++) {
-      var n = i * range + j
-      if (n != 0 && n != 1 && n != 2) {
-        args.push(n);
-      }
-    }
-    taskManager.addTask(primaryTest, args)
-  }
-
-  return this
+  return main.main()
 }
 
-module.exports.result = function(result) {
-  console.log('\n---------------------')
-  console.log(result.toString())
+module.exports.onResult = function(result) {
+  return onResult(result)
 }
---
+++ src/app/task/main.js
@@ -0,0 +1,42 @@
+exports.main = function() {
+
+  function primaryTest() {
+    var args = arguments[0]
+    var result = new Array()
+
+    for (var i = 0; i < args.length; i++) {
+      var num = args[i]
+      var flag = true
+      var limit = Math.sqrt(num)
+      for (var j = 2; j <= limit; j++) {
+        if (num % j == 0) {
+          flag = false
+          break
+        }   
+      }   
+      if (flag) {
+        result.push(num)
+      }   
+    }   
+    return result
+  }
+
+  var dataNum = 100000
+  var range = 100 
+  for (var i = 0; i < dataNum / range; i++) {
+    var args = new Array();
+    for (var j = 0; j < range; j++) {
+      var n = i * range + j 
+      if (n != 0 && n != 1 && n != 2) {
+        args.push(n);
+      }   
+    }   
+    #__PARALLEL__(primaryTest, args)
+  }
+  return this
+}
+
+exports.onResult = function(result) {
+  console.log('\n---------------------')
+  console.log(result.toString())
+}
